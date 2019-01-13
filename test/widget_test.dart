4a48ef50dc40911954f1835872d30d54f912e115
susumOyaji <sumitomo0210@gmail.com>
2019-01-13 15:24:07 +0900

    mac to up

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hotline1/main.dart';
+import 'package:hotline11/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
